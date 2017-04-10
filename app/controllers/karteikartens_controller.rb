cfd599a1658ce9b3d49c2e4f2840dd323116ebae
Prelang Builder <builder@prelang.com>
2017-04-10 00:10:05 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/karteikartens_controller.rb
+++ app/controllers/karteikartens_controller.rb
@@ -1,5 +1,5 @@
 class KarteikartensController < ApplicationController
-  before_action :set_karteikarten, only: [:show, :edit, :update, :destroy]
+  before_action :set_karteikarten, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /karteikartens
   # GET /karteikartens.json
