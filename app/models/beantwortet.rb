3768992cbfabeeea44cea55accaf8940a479eb24
Prelang Builder <builder@prelang.com>
2017-04-10 00:10:02 +0000

    Generated model Beantwortet with migration

---
+++ app/models/beantwortet.rb
@@ -0,0 +1,3 @@
+class Beantwortet < ActiveRecord::Base
+  belongs_to :user
+end
---
+++ test/models/beantwortet_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BeantwortetTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
